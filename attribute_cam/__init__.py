751f12b7cb8c31070ebb9a33bc5ff0759f23e8d4
Cedric Huber <chuber@rolf.ifi.uzh.ch>
2025-08-06 14:13:40 +0200

    deleted unused filter_majority file

--- attribute_cam/__init__.py
+++ attribute_cam/__init__.py
@@ -5,4 +5,3 @@ from .filter import Filter, read_list, prediction_file, FILTERS
 from .evaluation import statisics, error_rate, class_counts
 from .mask import get_masks, write_masks
 from .dataset_perturb import CelebA_perturb
-from .filter_majority import Filter1
